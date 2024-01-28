0a9ae85aaa3765cc08adb240d67d7e16f51a36a9
Emmaf97 <43826775+Emmaf97@users.noreply.github.com>
2024-01-28 16:24:37 +0000

    darkmode v2

--- js/darkMode.js
+++ js/darkMode.js
@@ -11,21 +11,24 @@ function darkMode() {
     let body = document.querySelector("body");
     let btnElement = document.querySelector(".btn");
     let btnElements = document.querySelectorAll(".btn-primary");
+    let footer = document.querySelector("#footer");
 
     if (btnElement) {
-        if(!toggle.classList.contains("active")){
+        if (!toggle.classList.contains("active")) {
             body.classList.remove("body-dark");
-            for(i=0; i < btnElements.length; i++){
-            console.log("test");
-            btnElements[i].classList.remove("button-dark");
+            footer.classList.remove("text-primaryDark");
+            footer.classList.add("text-primary");
+            for (i = 0; i < btnElements.length; i++) {
+                btnElements[i].classList.remove("button-dark");
             }
         }
         if (toggle.classList.contains("active")) {
-            console.log("clicked the buttin");
             body.classList.add("body-dark");
-            for(i=0; i < btnElements.length; i++){
+            footer.classList.add("text-primaryDark");
+            footer.classList.remove("text-primary");
+            for (i = 0; i < btnElements.length; i++) {
                 btnElements[i].classList.add("button-dark");
-                }
+            }
             // btnElements.classList.add("button-dark");
         }
     }
