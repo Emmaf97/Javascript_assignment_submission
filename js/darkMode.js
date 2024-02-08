f2090dd5a11ad52facec0820108765af0e326b8a
Emmaf97 <43826775+Emmaf97@users.noreply.github.com>
2024-02-08 22:01:32 +0000

    darkmode v2.2, cleanup of comments,css updates (mobile tablet)

--- js/darkMode.js
+++ js/darkMode.js
@@ -6,111 +6,56 @@
 //     }
 // });
 
-// function initRules() {
-//     let darkToggleRules = document.getElementById("darkModeBtnRules");
-//     darkToggleRules.addEventListener("click", darkMode);
-// }
-
 document.addEventListener("DOMContentLoaded", init);
-// document.addEventListener("DOMContentLoaded", checkDarkActive);
-// window.onload = function(){
-//     init();
-// }
 
 function init() {
     let darkToggleHome = document.getElementById("darkModeBtn");
     darkToggleHome.addEventListener("click", darkMode);
-    //darkToggleHome.addEventListener("click", checkDarkActive)
+    checkDarkActive();
 }
 
 let toggleCheck = false;
-function toggleCheckActive(){
-    return toggleCheck;
-}
+
 function checkDarkActive(){
-    let aElements = document.querySelectorAll(".btn-primary");
-    // if(aElements.classList.contains("button-dark")){
-    //     darkMode();
-    //     console.log("true value");
-    // } else {
-    //     darkMode();
-    //     console.log("false value");
-    // }
-    console.log(aElements);
-    //let {btnElements} = getIndexDocElements();
-    //     for (i = 0; i < btnElements.length; i++) {
-    //      btnElements[i].classList.toggle("button-dark");
-    //      if(toggleCheck === true){
-    //     }
-    // }
-    // else{
-    //     console.log("Not calling function correctly");
-    // }
+    if(localStorage.getItem("dark-mode") === "true"){
+      darkMode();
+    }
 }
 
 function darkMode() {
     let {btnElements} = getIndexDocElements();
+    let {label, legend} = uniqueDocElements();
     for (let i = 0; i < btnElements.length; i++) {
        let darkModeAdded =  btnElements[i].classList.toggle("button-dark");
-       if(darkModeAdded){
-        toggleCheck = true;
-        console.log(toggleCheck);
-        //return toggleCheck;
-        //checkDarkActive();
+       if(!darkModeAdded){
+        toggleCheck = false;
+        localStorage.setItem("dark-mode", toggleCheck);
        }
        else{
-        toggleCheck = false;
-        console.log(toggleCheck);
-        //return toggleCheck;
-        //checkDarkActive();
+        toggleCheck = true;
+        localStorage.setItem("dark-mode", toggleCheck);
        }
     }
-    darkModeCommonElements();
-    cardDark();
-    h2Dark();
-    pDark();
-    labelDark();
-    canvasDark();
-    //checkDarkActive();
-
-
-    // let { body, footer, mainNav} = getCommonDocElements();
-    // for (i = 0; i < btnElements.length; i++) {
-    //     btnElements[i].classList.toggle("button-dark");
-    //     //toggleCheck = true;
-    // }
-    // if(toggleCheck === true){
-    //     darkMode();
-
-    // } else {
-
-    // }
-
-    // let { toggleHome, body, btnElement, btnElements, footer, mainNav } = getIndexDocElements();
-    // if (btnElement) {
-        // if (!toggleHome.classList.contains("active")) {
-        //     // body.classList.toggle("body-dark");
-        //     // footer.classList.toggle("body-dark");
-        //     // mainNav.classList.toggle("body-dark");
-        //     // changeFootBdy(body, footer);
-        //     // changeButtons(btnElements);
-        //     // changeMainNav(mainNav);
-        // }
-        // if (toggleHome.classList.contains("active")) {
-            // body.classList.toggle("body-dark");
-            // footer.classList.toggle("text-primaryDark");
-            //mainNav.classList.toggle("mainNavDark");
-            //checkForActiveClass(toggleHome);
-            // revertFootBdy(body, footer);
-            // revertButtons(btnElements);
-            // revertMainNav(mainNav);
+    if(label && legend === null){
+        darkModeCommonElements();
+        cardDark();
+        h2Dark();
+        pDark();
+        canvasDark();
+    } else {
+        labelDark();
+        darkModeCommonElements();
+        cardDark();
+        h2Dark();
+        pDark();
+        canvasDark();
+    }
         }
-    // }
-// }
 
 function darkModeCommonElements(){
-    let { body} = getCommonDocElements();
+    let { body, toggleHome} = getCommonDocElements();
     body.classList.toggle("body-dark");
+    toggleHome.classList.add("active");
     // footer.classList.toggle("text-primaryDark");
 
 }
@@ -145,25 +90,6 @@ function labelDark(){
     legend.classList.toggle("legend-dark");
 }
 
-// function darkModeRules() {
-//     let { darkToggleRules, body, btnElement, footer } = getRulesDocElements();
-//     //let darkToggleRules = document.getElementById("darkModeBtnRules");
-
-//     if (btnElement) {
-//         if (!darkToggleRules.classList.contains("active")) {
-//             console.log("class active");
-//             darkToggleRules.classList.remove("active");
-//             changeFootBdy(body, footer);
-//             changeMainNav(mainNav);
-//         } else {
-//             console.log("No Class is active");
-//             revertFootBdy(body, footer);
-//             revertMainNav(mainNav);
-//             darkToggleRules.classList.add("active");
-//         }
-//     }
-// }
-
 function getCommonDocElements(){
     let toggleHome = document.getElementById("darkModeBtn");
     let body = document.querySelector("body");
@@ -184,14 +110,7 @@ function uniqueDocElements(){
 
 
 function getIndexDocElements() {
-    // let toggleHome = document.getElementById("darkModeBtn");
-    // let body = document.querySelector("body");
-    // let btnElement = document.querySelector(".btn");
     let btnElements = document.querySelectorAll(".btn-primary");
-    // let footer = document.querySelector("#footer");
-    // let mainNav = document.querySelectorAll(".mainNav");
-
-    // return { toggleHome, body, btnElement, btnElements, footer, mainNav };
     return {btnElements};
 
 }
@@ -206,43 +125,3 @@ function canvasDark(){
         ctx.fillStyle = "#0d6efd";
     }
 }
-// function getRulesDocElements() {
-//     let darkToggleRules = document.getElementById("darkModeBtnRules");
-//     let body = document.querySelector("body");
-//     let btnElement = document.querySelector(".btn");
-//     let footer = document.querySelector("#footer");
-//     let mainNav = document.querySelectorAll(".mainNav");
-//     return { darkToggleRules, body, btnElement, footer, mainNav };
-// }
-
-// function changeFootBdy(body, footer) {
-//     // body.classList.toggle("body-dark");
-//     // body.classList.remove("body-dark");
-//     footer.classList.remove("text-primaryDark");
-//     footer.classList.add("text-primary");
-// }
-// function revertFootBdy(body, footer) {
-//     body.classList.add("body-dark");
-//     footer.classList.add("text-primaryDark");
-//     footer.classList.remove("text-primary");
-// }
-// function changeButtons(btnElements) {
-//     for (i = 0; i < btnElements.length; i++) {
-//         btnElements[i].classList.remove("button-dark");
-//     }
-// }
-// function revertButtons(btnElements) {
-//     for (i = 0; i < btnElements.length; i++) {
-//         btnElements[i].classList.add("button-dark");
-//     }
-// }
-// function changeMainNav(mainNav) {
-//     for (j = 0; j < mainNav.length; j++) {
-//         mainNav[j].classList.remove("mainNavDark")
-//     }
-// }
-// function revertMainNav(mainNav) {
-//     for (j = 0; j < mainNav.length; j++) {
-//         mainNav[j].classList.add("mainNavDark");
-//     }
-// }
